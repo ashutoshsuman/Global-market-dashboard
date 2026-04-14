c233201b3b8164bdbcb5811d82280f25d4d264b3
ashutoshsuman <71302930+ashutoshsuman@users.noreply.github.com>
2026-04-14 22:26:22 +0530

    Initialize Streamlit app with basic functionality

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(page_title="NewsFlash App", layout="centered")
+
+st.title(" Streamlit App is Working")
+
+st.write("If you can see this, your deployment is successful.")
+
+name = st.text_input("Enter your name:")
+
+if name:
+
+    st.success(f"Hello {name}, your Streamlit app is live ")
+
+st.button("Click me")
